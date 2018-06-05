dd01ccf2b52e025d48328c72100660c8c4ad2b63
eric <Eric355402@yahoo.com>
2018-06-05 14:19:33 -0500

    added api request via axios on Search.js, SelectField on ln 42 only console.logs to 20 photos, SelectField not working properly

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
 
 class Search extends Component {
 	state = {
 		searchText: '',
-		amount: 15,
+		amount: 20,
 		apiUrl: 'https://pixabay.com/api',
 		apiKey: '9202240-89fbab64f3d613f515df41c52',
 		images: []
 	};
+
+	onTextChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value }, () => {
+			axios
+				.get(
+					`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+						this.state.searchText
+					}&image-type=photo&per-page=${this.state.amount}&safesearch=true`
+				)
+				.then((res) => this.setState({ images: res.data.hits }))
+				.catch((err) => console.log(err));
+		});
+	};
+
+	onAmountChange = (e, index, value) => this.setState({ amount: value });
 	render() {
+		console.log(this.state.images);
 		return (
 			<div>
 				<TextField
